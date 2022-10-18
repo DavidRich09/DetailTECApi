73e03a54d1c25da060510109c5945fd9c2c810bd
Sormocs <sermartbo@gmail.com>
2022-10-18 01:15:00 -0600

    New Model "LavadoProducto"

---
+++ P1API/P1API/Models/LavadoProducto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P1API.Models
+{
+    public partial class LavadoProducto
+    {
+        public string Nombre { get; set; } = null!;
+        public string Marca { get; set; } = null!;
+        public string TipoLavado { get; set; } = null!;
+        public int? Cantidad { get; set; }
+
+        public virtual Producto Producto { get; set; } = null!;
+        public virtual Lavado TipoLavadoNavigation { get; set; } = null!;
+    }
+}
