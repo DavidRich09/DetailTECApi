1bf68276324d418c06c465a9ba7d79511a3ee50a
David Richmond Soto <luis.richmond9@gmail.com>
2022-10-17 22:09:06 -0600

    ProveedorController.cs and SucursalController.cs

---
+++ P1API/P1API/Controllers/ProveedorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P1API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace P1API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProveedorController : ControllerBase
+    {
+        private readonly DetailTECContext context;
+
+        public ProveedorController(DetailTECContext context)
+        {
+            this.context = context;
+        }
+
+
+        [HttpPost]
+        [Route("saveSupplier")]
+        public ActionResult Post([FromBody] Proveedor value)
+        {
+
+            try
+            {
+                context.Add(value);
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+        
+        [HttpGet]
+        [Route("getSupplerisNames")]
+        public dynamic Get()
+        {
+            //hacer un select del nombre de la sucursal
+            var sucursales = context.Proveedors.Select(x =>  x.Nombre ).ToList();
+            return new
+            {
+                data = sucursales
+            };
+        }
+
+        
+        
+
+    }
+}
---
+++ P1API/P1API/Controllers/SucursalController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P1API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace P1API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SucursalController : ControllerBase
+    {
+
+        private readonly DetailTECContext context;
+
+        public SucursalController(DetailTECContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        [Route("saveOffice")]
+        public ActionResult Post([FromBody] Sucursal value)
+        {
+
+            try
+            {
+                context.Add(value);
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+
+
+
+    }
+
+}
