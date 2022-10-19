c4863bd2a3efc6c885ebbc50755c4cacb2da51dc
David Richmond Soto <luis.richmond9@gmail.com>
2022-10-18 23:05:53 -0600

    add controllers v2

---
+++ P1API/P1API/Controllers/ProductoController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using P1API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace P1API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        
+        private readonly DetailTECContext context;
+
+        public ProductoController(DetailTECContext context)
+        {
+            this.context = context;
+
+        }
+        
+        [HttpPost]
+        [Route("saveProduct")]
+        public ActionResult saveProduct([FromBody] Producto producto)
+        {
+            try
+            {
+                context.Add(producto);
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+        
+        
+        [HttpGet]
+        [Route("GetNamesProducts")]
+        public dynamic GetNamesProducts()
+        {
+            var products = context.Productos.Select(x => x.Nombre).ToList();
+            return new
+            {
+                data = products
+            };
+        }
+        
+    }
+}
---
+++ P1API/P1API/Controllers/ProvedorProductoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P1API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace P1API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProvedorProductoController : ControllerBase
+    {
+
+        private readonly DetailTECContext context;
+
+        public ProvedorProductoController(DetailTECContext context)
+        {
+            this.context = context;
+
+        }
+        
+        [HttpPost]
+        [Route("saveProvedorProducto")]
+        public ActionResult saveProduct([FromBody] ProveedorProducto value)
+        {
+
+            try
+            {
+                context.Add(value);
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
