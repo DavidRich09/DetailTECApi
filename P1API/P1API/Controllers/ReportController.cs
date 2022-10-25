9dc7dc3c74baa67df2c1c8b6122926bf0a572ad5
Ingrid Vargas Badilla <invabadilla@estudiantec.cr>
2022-10-24 19:09:42 -0600

    Reportes

---
+++ P1API/P1API/Controllers/ReportController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using P1API.Models;
+
+using System.Data;
+using CrystalDecisions.CrystalReports.Engine;
+
+
+
+
+
+
+
+namespace P1API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReportController : Controller
+    {
+        private readonly DetailTECContext context;
+
+        public ReportController(DetailTECContext context)
+        {
+            this.context = context;
+        }
+
+
+
+        [HttpGet]
+        [Route("Asistente")]
+
+        public dynamic Asistente()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), @"Execute\setup.exe");
+
+            System.Diagnostics.Process.Start(path);
+
+
+            return Ok();
+        }
+
+
+
+    }
+}
